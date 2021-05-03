3d9714f9b7d5fc6f98e5b139416f8a837dc77d6b
Joytun Tonny <66865972+joytun-tonny@users.noreply.github.com>
2021-05-03 18:03:56 +0600

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,30 @@
+
+
+// Array of Objects
+const quiz = [
+    {
+        q: 'Which month is your love anniversary ?',
+        options: ['may', 'september', 'july', 'august'],
+        answer:3
+    },
+    {
+        q: 'What color is a banana?',
+        options: ['red', 'yellow', 'white', 'blue'],
+        answer:1
+    },
+    {
+        q: 'Tonny is your Girlfriend?',
+        options: ['true', 'false'],
+        answer:0
+    },
+    {
+        q: 'What time of day your dick stays harder?',
+        options: ['In the night', 'In the evening', 'In the morning'],
+        answer:2
+    },
+    {
+        q: 'What is 22 + 6?',
+        options: ['99', '56', '16', '28'],
+        answer:3
+    }
+]
